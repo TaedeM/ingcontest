5e56b055640ff11acf1b813c91bbd8def39b23e3
TaedeM <78475736+TaedeM@users.noreply.github.com>
2021-12-16 18:09:12 +0100

    Add files via upload

--- DocumentProcessor.py
+++ DocumentProcessor.py
@@ -14,14 +14,38 @@ class DocumentProcessor:
         _contractNumber =  self.__filterContractNumberFromLine(_lineWithContractNumber)
         return _contractNumber
 
-    def __findLineContractNumber(self,stringList):
-        print("fineLine")
-        for line in stringList:
+    def __findLineContractNumber(self, stringList):
+        for line in stringList[:9]:
             if "Contractnummer" in line:
+                return line
+            elif "Leningnummer" in line:
+                return line
+            elif "Contractnr" in line:
+                return line
+            elif "contractnr" in line:
+                return line
+            elif "contractnummer" in line:
+                return line
+            elif "INGnummer" in line:
+                return line
+            elif "Ons kenmerk" in line:
                 return line
         return "not found"
 
     def __filterContractNumberFromLine(self,line):
+        #print(line, line.find("Datum:"))
+        if "Datum:" in line:
+            line = line[:line.find("Datum:")]    
+        line = line.replace(":", "")
+        line = line.replace(".", "")
+        line = line.replace(" ", "")
         line = line.replace("Contractnummer", "")
+        line = line.replace("contractnummer", "")
+        line = line.replace("Contractnummer:", "")
+        line = line.replace("Leningnummer", "")
+        line = line.replace("Contractnr", "")
+        line = line.replace("contractnr", "")
+        line = line.replace("INGnummer", "")
+        line = line.replace("Ons kenmerk", "")
         line = line.strip()
         return line
